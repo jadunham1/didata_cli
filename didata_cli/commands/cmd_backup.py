dd4892ed93a9e78f2fb275931540e805daab7d4d
Jeffrey Dunham <jeffrey.a.dunham@gmail.com>
2016-02-12 14:09:07 -0500

    Adding in listing storage/schedule/client policies to backups

--- didata_cli/commands/cmd_backup.py
+++ didata_cli/commands/cmd_backup.py
@@ -9,7 +9,7 @@ def cli(config):
     pass
 
 @cli.command()
-@click.option('--serverId', help='The server ID to enable backups on')
+@click.option('--serverId', type=click.UNPROCESSED, help='The server ID to enable backups on')
 @click.option('--servicePlan', required=True, help='The type of service plan to enroll in', type=click.Choice(['Enterprise', 'Essentials', 'Advanced']))
 @click.option('--serverFilterIpv6', help='The filter for ipv6')
 @pass_client
@@ -24,7 +24,7 @@ def enable(client, serverid, serviceplan, serverfilteripv6):
         handle_dd_api_exception(e)
 
 @cli.command()
-@click.option('--serverId', help='The server ID to disable backups on')
+@click.option('--serverId', type=click.UNPROCESSED, help='The server ID to disable backups on')
 @click.option('--serverFilterIpv6', help='The filter for ipv6')
 @pass_client
 def disable(client, serverid, serverfilteripv6):
@@ -40,7 +40,7 @@ def disable(client, serverid, serverfilteripv6):
         handle_dd_api_exception(e)
 
 @cli.command()
-@click.option('--serverId', help='The server ID to disable backups on')
+@click.option('--serverId', type=click.UNPROCESSED, help='The server ID to disable backups on')
 @click.option('--serverFilterIpv6', help='The filter for ipv6')
 @pass_client
 def info(client, serverid, serverfilteripv6):
@@ -68,7 +68,7 @@ def info(client, serverid, serverfilteripv6):
         handle_dd_api_exception(e)
 
 @cli.command(help='Adds a backup client')
-@click.option('--serverId', help='The server ID to list backup schedules for')
+@click.option('--serverId', type=click.UNPROCESSED, help='The server ID to list backup schedules for')
 @click.option('--clientType', required=True, help='The server ID to list backup schedules for')
 @click.option('--storagePolicy', required=True, help='The server ID to list backup schedules for')
 @click.option('--schedulePolicy', required=True, help='The server ID to list backup schedules for')
@@ -86,7 +86,7 @@ def add_client(client, serverid, clienttype, storagepolicy, schedulepolicy, trig
         handle_dd_api_exception(e)
 
 @cli.command(help='Removes a backup client')
-@click.option('--serverId', help='The server ID to list backup schedules for')
+@click.option('--serverId', type=click.UNPROCESSED, help='The server ID to list backup schedules for')
 @click.option('--clientType', required=True, help='The server ID to list backup schedules for')
 @click.option('--serverFilterIpv6', help='The filter for ipv6')
 @pass_client
@@ -110,7 +110,7 @@ def remove_client(client, serverid, clienttype, serverfilteripv6):
 
 
 @cli.command(help='Fetch Download URL for Server')
-@click.option('--serverId', help='The server ID to list backup schedules for')
+@click.option('--serverId', type=click.UNPROCESSED, help='The server ID to list backup schedules for')
 @click.option('--serverFilterIpv6', help='The filter for ipv6')
 @pass_client
 def download_url(client, serverid, serverfilteripv6):
@@ -125,3 +125,56 @@ def download_url(client, serverid, serverfilteripv6):
     except DimensionDataAPIException as e:
         handle_dd_api_exception(e)
 
+@cli.command(help='List client types available for server')
+@click.option('--serverId', type=click.UNPROCESSED, help='The server ID to list backup client types for')
+@click.option('--serverFilterIpv6', help='The filter for ipv6')
+@pass_client
+def list_available_client_types(client, serverid, serverfilteripv6):
+    if not serverid:
+        serverid = get_single_server_id_from_filters(client, ex_ipv6=serverfilteripv6)
+    try:
+        client_types = client.backup.ex_list_available_client_types(serverid)
+        if len(client_types) < 1:
+            click.secho("No available clients types for {0}".format(serverid), fg='red', bold=True)
+            exit(1)
+        click.secho("Available Client Types:", bold=True)
+        for client_type in client_types:
+            click.secho("{0}".format(client_type.type))
+    except DimensionDataAPIException as e:
+        handle_dd_api_exception(e)
+
+@cli.command(help='List schedule policies for server')
+@click.option('--serverId', type=click.UNPROCESSED, help='The server ID to list backup schedules for')
+@click.option('--serverFilterIpv6', help='The filter for ipv6')
+@pass_client
+def list_available_schedule_policies(client, serverid, serverfilteripv6):
+    if not serverid:
+        serverid = get_single_server_id_from_filters(client, ex_ipv6=serverfilteripv6)
+    try:
+        schedules = client.backup.ex_list_available_schedule_policies(serverid)
+        if len(schedules) < 1:
+            click.secho("No available schedules for {0}".format(serverid), fg='red', bold=True)
+            exit(1)
+        click.secho("Available Schedule Policies:", bold=True)
+        for schedule in schedules:
+            click.secho("{0}".format(schedule.name))
+    except DimensionDataAPIException as e:
+        handle_dd_api_exception(e)
+
+@cli.command(help='List storage policies for server')
+@click.option('--serverId', type=click.UNPROCESSED, help='The server ID to list backup storage polciies for')
+@click.option('--serverFilterIpv6', help='The filter for ipv6')
+@pass_client
+def list_available_storage_policies(client, serverid, serverfilteripv6):
+    if not serverid:
+        serverid = get_single_server_id_from_filters(client, ex_ipv6=serverfilteripv6)
+    try:
+        storage_policies = client.backup.ex_list_available_storage_policies(serverid)
+        if len(storage_policies) < 1:
+            click.secho("No available storage_policies for {0}".format(serverid), fg='red', bold=True)
+            exit(1)
+        click.secho("Available Storage Policies:", bold=True)
+        for storage_policy in storage_policies:
+            click.secho("{0}".format(storage_policy.name))
+    except DimensionDataAPIException as e:
+        handle_dd_api_exception(e)
