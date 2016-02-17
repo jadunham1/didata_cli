a396fc8ee759d935688464421934b4ef4ecc05be
Jeffrey Dunham <jeffrey.a.dunham@gmail.com>
2016-02-16 21:03:49 -0500

    Adding ability to delete/add network domains and list vlans

--- didata_cli/commands/cmd_network.py
+++ didata_cli/commands/cmd_network.py
@@ -1,6 +1,6 @@
 import click
 from didata_cli.cli import pass_client
-from libcloud.common.dimensiondata import DimensionDataAPIException
+from libcloud.common.dimensiondata import DimensionDataAPIException, DimensionDataNetworkDomain
 from didata_cli.utils import handle_dd_api_exception
 
 
@@ -8,6 +8,30 @@ from didata_cli.utils import handle_dd_api_exception
 @pass_client
 def cli(client):
     pass
+
+
+@cli.command()
+@click.option('--datacenterId', type=click.UNPROCESSED, help="Filter by datacenter Id")
+@click.option('--networkDomainId', type=click.UNPROCESSED, help="Filter by network domain")
+@pass_client
+def list_vlans(client, datacenterid, networkdomainid):
+    try:
+        if networkdomainid is not None:
+            networkdomainid = DimensionDataNetworkDomain(
+                                networkdomainid, None, None, None, None, None)
+        vlans = client.node.ex_list_vlans(
+            location=datacenterid,
+            network_domain=networkdomainid
+        )
+        for vlan in vlans:
+            click.secho("{0}".format(vlan.name), bold=True)
+            click.secho("ID: {0}".format(vlan.id))
+            click.secho("Description: {0}".format(vlan.description))
+            click.secho("IPv4 Range: {0}/{1}".format(vlan.private_ipv4_range_address, vlan.private_ipv4_range_size))
+            click.secho("IPv6 Range: {0}/{1}".format(vlan.ipv6_range_address, vlan.ipv6_range_size))
+            click.secho("")
+    except DimensionDataAPIException as e:
+        handle_dd_api_exception(e)
 
 
 @cli.command()
@@ -30,6 +54,35 @@ def list_network_domains(client, datacenterid):
 
 @cli.command()
 @click.option('--datacenterId', required=True, type=click.UNPROCESSED, help="Location for the network domain")
+@click.option('--name', required=True, help="Name for the network")
+@click.option('--servicePlan', required=True, help="Service plan")
+@click.option('--description', help="Description for the network domain")
+@pass_client
+def create_network_domain(client, datacenterid, name, serviceplan, description):
+    try:
+        client.node.ex_create_network_domain(datacenterid, name, serviceplan, description=description)
+        click.secho("Network Domain {0} created in {1}".format(name, datacenterid), fg='green', bold=True)
+    except DimensionDataAPIException as e:
+        handle_dd_api_exception(e)
+
+
+@cli.command()
+@click.option('--networkDomainId', required=True, help="ID of the network domain to remove")
+@pass_client
+def delete_network_domain(client, networkdomainid):
+    try:
+        client.node.ex_delete_network_domain(
+            DimensionDataNetworkDomain(
+                networkdomainid, None, None, None, None, None
+            )
+        )
+        click.secho("Network Domain {0} deleted.".format(networkdomainid), fg='green', bold=True)
+    except DimensionDataAPIException as e:
+        handle_dd_api_exception(e)
+
+
+@cli.command()
+@click.option('--datacenterId', required=True, type=click.UNPROCESSED, help="Location for the network")
 @click.option('--name', required=True, help="Name for the network")
 @click.option('--servicePlan', required=True, help="Service plan")
 @pass_client
