0fc91287d270c7a0bfc3699daa00d3c679ce53f6
Jeffrey Dunham <jeffrey.a.dunham@gmail.com>
2016-03-19 15:06:53 -0400

    Moving to assertTrue from assertIn for py2.6

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -24,14 +24,14 @@ class DimensionDataCLITestCase(unittest.TestCase):
     def test_server_list(self, node_client):
         node_client.return_value.list_nodes.return_value = load_dd_obj('node_list.json')
         result = self.runner.invoke(cli, ['server', 'list'])
-        self.assertIn('Private IPv4: 172.16.2.8', result.output)
+        self.assertTrue('Private IPv4: 172.16.2.8', result.output)
         self.assertEqual(result.exit_code, 0)
 
     def test_server_list_idsonly(self, node_client):
         node_client.return_value.list_nodes.return_value = load_dd_obj('node_list.json')
         result = self.runner.invoke(cli, ['server', 'list', '--idsonly'])
-        self.assertNotIn('Private IPv4: 172.16.2.8', result.output)
-        self.assertIn('b4ea8995-43a1-4b56-b751-4107b5671713', result.output)
+        self.assertFalse('Private IPv4: 172.16.2.8' in result.output)
+        self.assertTrue('b4ea8995-43a1-4b56-b751-4107b5671713' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_create_node(self, node_client):
@@ -43,7 +43,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
                                      '--description', 'fakedescription',
                                      '--networkDomainId', 'b53b2ad4-ca8b-4abd-9140-72d6b137a6b4',
                                      '--vlanId', 'f04e4e2c-a52e-45d3-8037-9f1b1e234c05'])
-        self.assertIn('8aeff10c-c918-4021-b2ce-93e4a209418b', result.output)
+        self.assertTrue('8aeff10c-c918-4021-b2ce-93e4a209418b' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_create_node_APIException(self, node_client):
@@ -56,7 +56,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
                                      '--description', 'fakedescription',
                                      '--networkDomainId', 'b53b2ad4-ca8b-4abd-9140-72d6b137a6b4',
                                      '--vlanId', 'f04e4e2c-a52e-45d3-8037-9f1b1e234c05'])
-        self.assertIn('REASON 541', result.output)
+        self.assertTrue('REASON 541' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_info(self, node_client):
@@ -64,7 +64,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'info',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('ID: 8aeff10c-c918-4021-b2ce-93e4a209418b', result.output)
+        self.assertTrue('ID: 8aeff10c-c918-4021-b2ce-93e4a209418b' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_update_ram(self, node_client):
@@ -74,7 +74,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
                                     ['server', 'update_ram',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b',
                                      '--ramInGB', '8'])
-        self.assertIn('changed to 8GB', result.output)
+        self.assertTrue('changed to 8GB' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_update_ram_server_filters(self, node_client):
@@ -84,7 +84,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
                                     ['server', 'update_ram',
                                      '--serverFilterIpv6', '::1',
                                      '--ramInGB', '8'])
-        self.assertIn('changed to 8GB', result.output)
+        self.assertTrue('changed to 8GB' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_update_ram_APIException(self, node_client):
@@ -95,7 +95,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
                                     ['server', 'update_ram',
                                      '--serverFilterIpv6', '::1',
                                      '--ramInGB', '8'])
-        self.assertIn('REASON 540', result.output)
+        self.assertTrue('REASON 540' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_update_cpu_count(self, node_client):
@@ -105,7 +105,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
                                     ['server', 'update_cpu_count',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b',
                                      '--cpuCount', '4'])
-        self.assertIn('CPU Count changing to 4', result.output)
+        self.assertTrue('CPU Count changing to 4' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_update_cpu_count_server_filters(self, node_client):
@@ -115,7 +115,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
                                     ['server', 'update_cpu_count',
                                      '--serverFilterIpv6', '::1',
                                      '--cpuCount', '4'])
-        self.assertIn('CPU Count changing to 4', result.output)
+        self.assertTrue('CPU Count changing to 4' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_update_cpu_count_APIException(self, node_client):
@@ -126,7 +126,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
                                     ['server', 'update_cpu_count',
                                      '--serverFilterIpv6', '::1',
                                      '--cpuCount', '4'])
-        self.assertIn('REASON 539', result.output)
+        self.assertTrue('REASON 539' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_destroy(self, node_client):
@@ -135,7 +135,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'destroy',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('is being destroyed', result.output)
+        self.assertTrue('is being destroyed' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_destroy_server_filters(self, node_client):
@@ -144,7 +144,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'destroy',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('is being destroyed', result.output)
+        self.assertTrue('is being destroyed' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_destroy_return_False(self, node_client):
@@ -153,7 +153,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'destroy',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('Something went wrong with attempting to destroy', result.output)
+        self.assertTrue('Something went wrong with attempting to destroy' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_destroy_APIException(self, node_client):
@@ -163,7 +163,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'destroy',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('REASON 538', result.output)
+        self.assertTrue('REASON 538' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_reboot(self, node_client):
@@ -172,7 +172,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'reboot',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('is being rebooted', result.output)
+        self.assertTrue('is being rebooted' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_reboot_server_filters(self, node_client):
@@ -181,7 +181,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'reboot',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('is being rebooted', result.output)
+        self.assertTrue('is being rebooted' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_reboot_return_False(self, node_client):
@@ -190,7 +190,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'reboot',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('Something went wrong with attempting to reboot', result.output)
+        self.assertTrue('Something went wrong with attempting to reboot' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_reboot_APIException(self, node_client):
@@ -200,7 +200,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'reboot',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('REASON 537', result.output)
+        self.assertTrue('REASON 537' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_reboot_hard(self, node_client):
@@ -209,7 +209,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'reboot_hard',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('is being rebooted', result.output)
+        self.assertTrue('is being rebooted' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_reboot_hard_server_filters(self, node_client):
@@ -218,7 +218,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'reboot_hard',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('is being rebooted', result.output)
+        self.assertTrue('is being rebooted' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_reboot_hard_return_False(self, node_client):
@@ -227,7 +227,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'reboot_hard',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('Something went wrong with attempting to reboot', result.output)
+        self.assertTrue('Something went wrong with attempting to reboot' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_reboot_hard_APIException(self, node_client):
@@ -237,7 +237,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'reboot_hard',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('REASON 536', result.output)
+        self.assertTrue('REASON 536' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_shutdown(self, node_client):
@@ -246,7 +246,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'shutdown',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('is shutting down gracefully', result.output)
+        self.assertTrue('is shutting down gracefully' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_shutdown_server_filters(self, node_client):
@@ -255,7 +255,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'shutdown',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('is shutting down gracefully', result.output)
+        self.assertTrue('is shutting down gracefully' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_shutdown_return_False(self, node_client):
@@ -264,7 +264,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'shutdown',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('Something went wrong when attempting to shutdown', result.output)
+        self.assertTrue('Something went wrong when attempting to shutdown' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_shutdown_APIException(self, node_client):
@@ -274,7 +274,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'shutdown',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('REASON 535', result.output)
+        self.assertTrue('REASON 535' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_shutdown_hard(self, node_client):
@@ -283,7 +283,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'shutdown_hard',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('is shutting down hard', result.output)
+        self.assertTrue('is shutting down hard' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_shutdown_hard_server_filters(self, node_client):
@@ -292,7 +292,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'shutdown_hard',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('is shutting down hard', result.output)
+        self.assertTrue('is shutting down hard' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_shutdown_hard_return_False(self, node_client):
@@ -301,7 +301,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'shutdown_hard',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('Something went wrong when attempting to shutdown', result.output)
+        self.assertTrue('Something went wrong when attempting to shutdown' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_shutdown_hard_APIException(self, node_client):
@@ -311,7 +311,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'shutdown_hard',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('REASON 534', result.output)
+        self.assertTrue('REASON 534' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_start(self, node_client):
@@ -320,7 +320,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'start',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('is starting', result.output)
+        self.assertTrue('is starting' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_start_server_filters(self, node_client):
@@ -329,7 +329,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'start',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('is starting', result.output)
+        self.assertTrue('is starting' in result.output)
         self.assertTrue(result.exit_code == 0)
 
     def test_server_start_return_False(self, node_client):
@@ -338,7 +338,7 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'start',
                                      '--serverFilterIpv6', '::1'])
-        self.assertIn('Something went wrong when attempting to start', result.output)
+        self.assertTrue('Something went wrong when attempting to start' in result.output)
         self.assertTrue(result.exit_code == 1)
 
     def test_server_start_APIException(self, node_client):
@@ -348,5 +348,5 @@ class DimensionDataCLITestCase(unittest.TestCase):
         result = self.runner.invoke(cli,
                                     ['server', 'start',
                                      '--serverId', '8aeff10c-c918-4021-b2ce-93e4a209418b'])
-        self.assertIn('REASON 533', result.output)
+        self.assertTrue('REASON 533' in result.output)
         self.assertTrue(result.exit_code == 1)
